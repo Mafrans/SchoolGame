b06d1c9dab41e217b3c10e21187bed5d525d3da4
Mafrans <malte.kluft@gmail.com>
2020-05-07 10:16:32 +0200

    Add offset to circle collider

--- src/me/mafrans/game/CircleCollider.java
+++ src/me/mafrans/game/CircleCollider.java
@@ -2,10 +2,11 @@ package me.mafrans.game;
 
 public class CircleCollider implements Collider {
     public int radius = 0;
+    public Vector2 offset = Vector2.one.dot(radius);
 
     @Override
     public Collision withCircle(Vector2 a, Vector2 b, CircleCollider c) {
-        double distance = Vector2.distance(a, b);
+        double distance = Vector2.distance(a.add(offset), b);
 
         if(distance > radius + c.radius) {
             return null;
