a7512d2d034020b17da013292d78242edec4c412
Mafrans <malte.kluft@gmail.com>
2020-05-07 10:39:27 +0200

    Fix issue where the game crashes if an object goes out of bounds

--- src/me/mafrans/game/GameObject.java
+++ src/me/mafrans/game/GameObject.java
@@ -15,7 +15,10 @@ public abstract class GameObject {
 
         for (int i = 0; i < sprite.getHeight(); i++) {
             for (int j = 0; j < sprite.getWidth(); j++) {
-                screen[(int)((transform.position.y+i)*screenWidth + transform.position.x+j)] = sprite.getPixels()[i*sprite.getWidth() + j];
+                int index = (int)((transform.position.y+i)*screenWidth + transform.position.x+j);
+                if(index >= 0 && index < screen.length) {
+                    screen[index] = sprite.getPixels()[i*sprite.getWidth() + j];
+                }
             }
         }
     }
