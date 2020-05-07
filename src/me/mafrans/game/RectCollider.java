c55c4545c5269901dcf881e496f00e0e744a28c4
Mafrans <malte.kluft@gmail.com>
2020-05-07 10:17:40 +0200

    Create rectangle collider

--- src/me/mafrans/game/RectCollider.java
+++ src/me/mafrans/game/RectCollider.java
@@ -1,17 +1,11 @@
 package me.mafrans.game;
 
 public class RectCollider implements Collider {
-    public int radius = 0;
+    public Vector2 dimension;
+    public Vector2 offset;
 
     @Override
     public Collision withCircle(Vector2 a, Vector2 b, CircleCollider c) {
-        double distance = Vector2.distance(a, b);
-
-        if(distance > radius + c.radius) {
-            return null;
-        }
-        else {
-            return new Collision(b.subtract(a).normalize());
-        }
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
